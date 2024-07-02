c77e58b04bb4a776f342b50172c6c0ebb119ecf3
Naveen Prajapati <140879750+Naveen7003@users.noreply.github.com>
2024-07-02 21:31:33 +0530

    Update Home.jsx with horizontal card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,11 @@ import Header from "./partials/Header"
 import HorizontalCards from './partials/HorizontalCards'
 
 const Home = () => {
-    document.title = "VideoApp | HomePage"
-    const [wallpaper, setwallpaper ] = useState(null)
-    const GetHeaderWallpaper = async() => {
+  document.title = "VideoApp | HomePage"
+  const [wallpaper, setwallpaper ] = useState(null)
+  const [trending, settrending] = useState(null)
+
+    const GetHeaderWallpaper = async() => {             //fetched trending all day items for header
       try {
           const {data} = await axios.get(`/trending/all/day`)
           let randomData = data.results[(Math.random() * data.results.length).toFixed()]   //yha ek random and fixed no ayega which is less than 19
@@ -17,18 +19,29 @@ const Home = () => {
           console.log("Error: ",error)
       }
   }
-  
+
+    const Gettrending = async() => {                      //fetched trending items for cards
+    try {
+        const {data} = await axios.get(`/trending/all/day`)
+        settrending(data.results)
+    } catch (error) {
+        console.log("Error: ",error)
+    }
+  } 
 
   useEffect(()=> {
       !wallpaper && GetHeaderWallpaper()  //agr wallpaper mai kuch nhi hai tabhi call karna
+      !trending && Gettrending()          //agr trending mai kuch nhi hai tabhi call karna
   },[])
-  return wallpaper ? (
+
+
+  return wallpaper && trending ? (         //agr api se trending and wallpaper dono aa rhe hai tabhi show kro warna loading show kro
     <>
         <Sidenav />
-        <div className='h-full w-[80%] '>
+        <div className='h-full w-[80%] overflow-auto overflow-x-hidden'>
             <Topnav />
             <Header data={wallpaper} />
-            <HorizontalCards />
+            <HorizontalCards data={trending} />
             
         </div>
     </>
