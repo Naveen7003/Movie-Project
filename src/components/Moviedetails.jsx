fc493f815da7976432432d0266c3df8e77ccba4f
Naveen Prajapati <140879750+Naveen7003@users.noreply.github.com>
2024-07-07 13:25:30 +0530

    fixed Moviedetails.jsx

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 
 const Moviedetails = () => {
   return (
-   <div className='w-full py-3 px-8  flex items-center'>
-            <h1 className='text-3xl font-semibold text-zinc-400'>
-                <i onClick={() => navigate(-1)} className='hover:text-[#6556cd] mr-3  ri-arrow-left-line'></i>{""}
-                People
-            </h1>
-            <Topnav />
-           
+   <div >
+            
         </div>
   )
 }
