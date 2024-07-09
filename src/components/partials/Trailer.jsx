f0e66bdcbffde845f779e867b6192d5659a60703
Naveen Prajapati <prajapatinaveen7003@gmai.com>
2024-07-09 11:10:02 +0530

    trailer

--- src/components/partials/Trailer.jsx
+++ src/components/partials/Trailer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Trailer = () => {
   return (
-    <div>Trailer</div>
+    <div className='bg-[rgba(0,0,0,.9)] absolute z-100  top-0 left-0 w-screen h-screen flex items-center justify-center'>Trailer</div>
   )
 }
 
